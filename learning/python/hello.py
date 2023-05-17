517fbb00d9989d5af06c1854fce7790e43ad0713
yangfx15 <yangfx15@foxmail.com>
2023-05-17 10:07:00 +0800

    thing

--- learning/python/hello.py
+++ learning/python/hello.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from enum import Enum
-@unique
-class Weekday(Enum):
-    Sun = 0
-    Mon = 1
-    Tue = 2
-    Wed = 3
-    Thu = 4
-    Fri = 5
-    Sat = 6
-
-day1 = Weekday.Mon
-print(day1)
-
-Weekday.Mon
+class Hello(object):
+    def hello(self, name='world'):
+        print('Hello %s!' % name)
