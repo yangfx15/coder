9d3fd1e0901133395201fb82707fb04851a612f0
yangfengxin <yangfengxin@xverse.cn>
2023-08-11 22:08:00 +0800

    python入门（下）

--- learning/python/main.py
+++ learning/python/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+# 新增一个书籍Book的数据结构，继承自BaseModel类
+class Book(BaseModel):
+    name: str
+    desp: str
+    price: float
 
 app = FastAPI()
-
-@app.get("/")
-def hello():
-    return {"Hello":"world"}
+# 定义一个post接口，接收一本书的信息
+@app.post("/books/")
+async def create_book(book: Book):
+    return book
